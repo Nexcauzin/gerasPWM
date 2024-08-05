07920e67f072009461f7a6e63f1db48ce2e7f773
Nexcauzin <eusoutaylann@gmail.com>
2024-08-05 19:16:06 -0300

    setupPWM e setDutyCycle criados

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+uint16_t TOP;
+
+void setup_fastPWM(){
+  // Configurado para gerar no pino Digital 9 (OCR1A)
+  // Está no Port D, bit PD6
+  // Configurando o pino OC1A (PD6) como saída
+  DDRD |= (1 << DDD6);
+
+  // Frequências possíveis com Fast PWM (N=Prescale)
+  // f_pwm = f_clk/(N*256)
+  //       = 16mHz/256N
+  // Como mudar o prescale:
+  // CS12 CS11 CS10 -> Prescaler -> f_pwm (considerando 16MHz de entrada)
+  //   0   0    0   -> Inativo   -> -
+  //   0   0    1   ->   1       -> 16 MHz
+  //   0   1    0   ->   8       -> 2 MHz
+  //   0   1    1   ->   64      -> 250 KHz
+  //   1   0    0   ->   256     -> 62,5 KHz
+  //   1   0    1   ->   1024    -> 15,625 KHz
+
+  // Configurando o Timer/Counter1 para Fast PWM
+  TCCR1B |= (1 << CS11);
+  // Essa config trás a FREQUÊNCIA BASE do PWM, então dá pra alterar com o TOP qual vai ser
+  // A freq final do pwm
+
+  // O TOP do PWM (IMPORTANTE ISSO HEIN) pode ser calculado como:
+  // TOP = (f_clk/(N*f_pwm)) - 1
+  // Dessa vez, o f_pwm vai ser a frequência final do PWM, a que vai ser gerada
+  // O valor do ICR1 deve ser definido com o valor do TOP
+
+  // Ex: Freq de 10416Hz: 
+  // Seleciona-se o Prescale de 256, no fim o cálculo de TOP entregará TOP=5
+  // Equivalendo a 10,416 KHz
+
+  // Deve ser sempre um valor INT
+  ICR1 = 199; 
+  uint16_t TOP = ICR1; // Pra usar na função que altera o Duty Cycle
+  OCR1A = 0; 
+}
+
+void setDutyCycle(int porcentPWM){
+  // porcentPWM deve estar entre 0-100
+  // Essa função lê valores de 0-100 e faz o Duty Cycle adotar esse valor
+  if (porcentPWM < 0){
+    porcentPWM = 0;
+  }
+
+  if (porcentPWM > 100){
+    porcentPWM = 100;
+  }
+
+  uint16_t dutyCycle = (porcentPWM * (TOP + 1))/100; // Cálculo do ICR para % do Duty
+  ICR1 = dutyCycle;
+}
+
+int main(void){
+  setup_fastPWM();
+
+  while (1){
+  }
+}
