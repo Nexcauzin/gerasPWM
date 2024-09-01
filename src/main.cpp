4bebc3df8a4451e89d7079f2536ee4b9aaf35e91
Taylan Mayckon <130873668+Nexcauzin@users.noreply.github.com>
2024-09-01 20:25:24 -0300

    Comando lateral implementado (protótipo)
    Falta implementar a leitura dos sinais e validar esse comando lateral

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <util/delay.h>
 
 uint8_t TOP = 255; // Se for usar Timer1 isso deve ser = a ICR1 e tem que ser 16bit
+uint16_t inputThrottle, inputLateral, inputFreio;
 
 // Funçãozinha marota só pra ficar elegante lá embaixo
 float limitasValores(float valor, float minimo, float maximo){
@@ -60,7 +61,7 @@ float limitasValores(float valor, float minimo, float maximo){
 
 
 void setup_fastPWM(){
-  // Pinos D5 e D6
+  // Pinos D5 e D6 (configurando como output)
   DDRD |= (1 << DDD5 | 1 << DDD6);
 
   // Configurando o Timer0 para Fast PWM não-invertido
@@ -77,23 +78,97 @@ uint8_t setDutyCycle(float porcentPWM){
   // Essa função lê valores de 0-100 e faz o Duty Cycle adotar esse valor
 
   // Isso é para limitar o valor mínimo do Duty Cycle
-  porcentPWM = limitasValores(porcentPWM, 0, 100);
+  porcentPWM = limitasValores(porcentPWM, 0, 1);
 
-  uint8_t dutyCycle = (uint8_t)((porcentPWM/100) * TOP);
+  uint8_t dutyCycle = (uint8_t)((porcentPWM) * TOP);
   return dutyCycle;
 }
 
 
 int main(void){
   setup_fastPWM();
+  
+  // BASEADO NA PONTE H L298N
+  // TABELA VERDADE:
+  // IN1   IN2   -> SENTIDO
+  //  0     1    -> FREIO DE MÃO
+  //  1     0    -> Frente
+  //  0     1    -> Ré
+  //  1     1    -> FREIO DE MÃO
+
+  // PINOS UTILIZADOS PRA DIREÇÃO (configurando como input)
+  // Motor direito:
+  DDRD &= ~(1 << DDD4 | 1 << DDD7); // D4 e D7 
+  // IN1 = D4, IN2 = D7
+
+  // Motor esquerdo:
+  DDRB &= ~(1 << DDB4 | 1 << DDB5); // D12 e D13
+  // IN3 = D12, IN4 = D13
+
+  // Inicializando ambos freados
+  PORTD &= ~(1 << PD4 | 1 << PD7);
+  PORTB &= ~(1 << PB4 | 1 << PB5);
 
   while (1){
+    
+    // Aqui configura uma chave de ativação no controle, quando ela for ativa vai ter um valor x (botei >=1800 ficticio), e engatilha a operação
+    // em us, e a partir daí faz a operação de frenagem do robô (a ser pensada)
+    if (inputFreio >= 1800){
+      // por enquanto tá só travando os motores
+      PORTD &= ~(1 << PD4 | 1 << PD7);
+      PORTB &= ~(1 << PB4 | 1 << PB5);
+      // Se MADS passar algo gradual pra frenagem, cria um Flag pra ser acionado enquanto o gatilho estiver ativado
+      // Faz a frenagem gradual dos motores, e mantem o estado 0 até que o Flag seja liberado (gatilho desativado)
+    }
 
-    OCR0B = setDutyCycle(50);
-    OCR0A = setDutyCycle(50);
+    // Se não ativar a chave:
+    else{
+      // Essa etapa leva em consideração valor entre 1000 e 2000 us
+      // PROCESSAMENTO DO COMANDO FRENTE/TRÁS
+      // Aqui entra o valor lido do radiocontrole (1000 a 2000)
+      if (inputThrottle >= 1500){
+        inputThrottle = limitasValores((inputThrottle - 1500) * 0.002, 0, 1); // Traz o valor para o range 0 a 1 (float)
+        // ASSUMINDO AS ROTAÇÕES AÍ
+        // Motor DIREITO
+        PORTD &= ~(1 << PD4);
+        PORTD |= (1 << PD7);
 
-    _delay_ms(1000);
+        // Motor ESQUERDO
+        PORTD &= ~(1 << PB5);
+        PORTD |= (1 << PD7);
+      } 
 
+      else if (inputThrottle < 1500){
+        inputThrottle = limitasValores((1500 - inputThrottle) * 0.002, 0, 1);
+        // ASSUMINDO AS ROTAÇÕES AÍ
+        // Motor DIREITO
+        PORTD &= ~(1 << PD7);
+        PORTD |= (1 << PD4);
+
+        // Motor ESQUERDO
+        PORTD &= ~(1 << PB7);
+        PORTD |= (1 << PD5);
+      }
+      
+
+      // Tô levando em consideração que:
+      // OCR0A -> Motor DIREITO
+      // OCR0B -> Motor ESQUERDO
+
+      // PROCESSAMENTO DO COMANDO LATERAL
+      // DIREITA
+      if (inputLateral >= 1500){
+        inputLateral = limitasValores((inputLateral - 1500) * 0.002, 0, 1);
+        OCR0A = setDutyCycle(inputThrottle - inputLateral);
+        OCR0B = setDutyCycle(inputThrottle);
+      }
+      // ESQUERDA
+      else if (inputLateral < 1500){
+        inputLateral = limitasValores((1500 - inputLateral) * 0.002, 0, 1);
+        OCR0A = setDutyCycle(inputThrottle);
+        OCR0B = setDutyCycle(inputThrottle - inputLateral);
+      }
+    }
   }
 }
 
